c1ee3dfd6f9bdd9a597932296706a8e7d9d7551a
Yauhen Artsiukhou <yauhen_artsiukhou@epam.com>
2015-10-15 21:42:39 +0300

    Add compatibility for Chef 11

--- src/metadata.rb
+++ src/metadata.rb
@@ -4,6 +4,6 @@ maintainer_email '!{maintainer_email}'
 license          '!{license}'
 description      '!{description}'
 long_description '!{long_description}'
-issues_url       '!{issues_url}'
-source_url       '!{source_url}'
+issues_url       '!{issues_url}' if respond_to?(:issues_url)
+source_url       '!{source_url}' if respond_to?(:source_url)
 version          '!{version}'
