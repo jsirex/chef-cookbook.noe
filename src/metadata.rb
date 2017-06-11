b86249b8b309561864e2f541cac0f81f3e146493
Yauhen Artsiukhou <jsirex@gmail.com>
2017-06-11 16:41:01 +0300

    Update metadata style and entries

--- src/metadata.rb
+++ src/metadata.rb
@@ -5,16 +5,20 @@ maintainer       '!{maintainer}'
 maintainer_email '!{maintainer_email}'
 license          '!{license}'
 description      '!{description}'
-long_description '!{long_description}'
-issues_url       '!{issues_url}'
-source_url       '!{source_url}'
 version          '!{version}'
 
-# depends 'cookbook', '>= 0.0.0'
+issues_url       '!{issues_url}'
+source_url       '!{source_url}'
 
 # supports 'debian'
 # supports 'centos'
 
-chef_version '>= 12.12'
+# depends 'cookbook', '>= 0.0.0'
 
+# chef_version '>= 12.12'
+
+# Provided recipes
 recipe '!{name}::default', '!{description}'
+
+# Automatically installed gems before chef run (new in 12.8)
+# gem 'whatever'
